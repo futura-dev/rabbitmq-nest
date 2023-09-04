2b123c6970f6189664c831751dcc1758f73fdcf0
faridevnz <alessio.farinelli.developer@gmail.com>
2023-09-04 09:37:47 +0200

    chore: removed useless logs

--- src/nest-rabbitmq/nest-rabbitmq.service.ts
+++ src/nest-rabbitmq/nest-rabbitmq.service.ts
@@ -60,8 +60,8 @@ export class NestRabbitmqService<M> implements OnModuleInit {
           channel
             .assertQueue(this.QUEUE, this.options ?? {})
             .then(async (queue_assertion) => {
-              this.logger.log('connected in ', this.mode, 'mode to', this.rabbitUri);
-              this.logger.log('configured', `"${queue_assertion.queue}"`);
+              this.logger.log(`connected in "${this.mode}" mode to ${this.rabbitUri}`);
+              this.logger.log(`configured "${queue_assertion.queue}"`);
               resolve();
               // resolve({ ...options, mode: options.mode, channel: channel, queue_assertion: queue_assertion });
               // register listener
